2ebd95584d2b1385d403e9809e5ab8c0413c3baf
emmanuel <emmanuel.ortiz@carsales.com>
2021-06-26 23:16:02 -0400

    initial project

---
+++ projectFirebase/projectFirebase/projectFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectFirebaseApp.swift
+//  projectFirebase
+//
+//  Created by Emmanuel Ortiz Matus on 26-06-21.
+//
+
+import SwiftUI
+
+@main
+struct projectFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
